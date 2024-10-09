5f35f15ca7de266c7061c4103faf4a5650b1d3cd
Eric Kampman <erickampman@yahoo.com>
2024-10-09 07:24:45 -0700

    Initial Commit

---
+++ CollectionAddRemove/CollectionAddRemoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionAddRemoveApp.swift
+//  CollectionAddRemove
+//
+//  Created by Eric Kampman on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CollectionAddRemoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
